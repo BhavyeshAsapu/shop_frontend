e3bfc9efcddfaaf0b3f3be8107fd39ac64b6dcac
Bhavyesh <163726860+BhavyeshAsapu@users.noreply.github.com>
2025-09-21 23:12:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: '/frontapp1/',   // ✅ must end with a slash
+  plugins: [react(), tailwindcss()],
+  base: '/frontapp1/',   // 👈 must match Tomcat context path (with trailing slash)
 })
