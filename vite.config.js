5efa494880ffb28b7b94578b42fbc12ccef4a185
Bhavyesh <163726860+BhavyeshAsapu@users.noreply.github.com>
2025-09-21 23:11:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: '/frontapp1',   // 👈 put it here (outside plugins)
+  base: '/frontapp1/',   // ✅ must end with a slash
 })
