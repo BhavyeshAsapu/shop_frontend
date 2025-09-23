2da4f121415673bc8be1dbab0c38d6d761e373ad
Bhavyesh <163726860+BhavyeshAsapu@users.noreply.github.com>
2025-09-23 10:36:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/frontapp1/',   // or '/' if deployed as ROOT.war
-  server: {
-    port: 2004,
-    proxy: {
-      '/admin': {
-        target: 'http://localhost:9090/backendapp',
-        changeOrigin: true,
-      },
-      '/address': {
-        target: 'http://localhost:9090/backendapp',
-        changeOrigin: true,
-      },
-    },
-  },
+  plugins: [react(),
+    tailwindcss(),
+  ],
 })
